8078ce3ddf5f0c80b008a07a70407383c4da128c
psychemist <ikeagudike@gmail.com>
2025-08-28 19:39:12 +0100

    fix: change site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   authors: [{ name: site.name }],
   creator: site.name,
   icons: {
-    icon: '/brain.png',
+    icon: '/brains.png',
   },
   manifest: '/site.webmanifest',
   openGraph: {
